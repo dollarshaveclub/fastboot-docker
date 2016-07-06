9714ef48ccbe1357f704b533bddea2de3f2203c3
Patrick Fisher <patrick@pwfisher.com>
2016-07-06 16:13:08 -0700

    Add fastboot-watch-notifier

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
+"use strict";
+
 const FastBootAppServer = require('fastboot-app-server');
+const FastBootWatchNotifier = require('fastboot-watch-notifier');
+
+const distPath = '/app/dist';
+
+const notifier = new FastBootWatchNotifier({
+  distPath,
+  saneOptions: {
+    poll: true,
+  },
+});
 
 const server = new FastBootAppServer({
-  distPath: '/app/dist'
+  distPath,
+  notifier,
 });
 
 server.start();
