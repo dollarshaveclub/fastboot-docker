68a5f8df0ee8889339b62f67c287b1f90f2d58f2
Brian Gonzalez <me@briangonzalez.org>
2016-08-15 21:14:26 -0700

    Now using initial middleware

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@
 
 const FastBootAppServer = require('fastboot-app-server');
 const FastBootWatchNotifier = require('fastboot-watch-notifier');
-const isFastBootRoute = require('./is-fastboot-route');
 
 const distPath = '/app/dist';
 
@@ -13,25 +12,13 @@ const notifier = new FastBootWatchNotifier({
   },
 });
 
-const userMiddleware = function (req, res, next) {
-
-  let doFastBoot = isFastBootRoute(req.path);
-  if (req.query.fastboot === 'on') doFastBoot = true;
-  if (req.query.fastboot === 'off') doFastBoot = false;
-
-  if (doFastBoot) {
-    next();
-  } else {
-    res.sendFile('index.html', { root: distPath });
-  }
-};
-
+const initialMiddleware = require('./middleware');
 const workerCount = process.env.WORKER_COUNT;
 
 const server = new FastBootAppServer({
   distPath,
   notifier,
-  userMiddleware,
+  initialMiddleware,
   workerCount,
 });
 
