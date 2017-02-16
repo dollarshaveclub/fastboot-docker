9e299c2c7231fff8599fcc5ae77eca590aeb6587
Brian Gonzalez <me@briangonzalez.org>
2017-02-16 08:38:13 -0800

    Fix tests and move CI tasks to shell script to enable easy local testing

--- server-fastboot-docker/middleware/before-fastboot.js
+++ server-fastboot-docker/middleware/before-fastboot.js
@@ -1,8 +1,9 @@
 "use strict";
 
+const express = require('express');
 const distPath = 'app/dist';
 const isFastbootRoute = require('./is-fastboot-route');
-const staticAssetOptions = require('./config/static-assets.js');
+const staticAssetOptions = require('../config/static-assets.js');
 
 function customMiddleware (req, res, next) {
   let doFastBoot = isFastbootRoute(req.baseUrl);
@@ -17,6 +18,7 @@ function customMiddleware (req, res, next) {
 };
 
 module.exports = function (app) {
+  app.use(require('compression')());
   app.use(express.static(distPath, staticAssetOptions));
   app.use('/*', customMiddleware);
 };
